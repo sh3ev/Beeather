2ee1d0128f23a91b5fb3cdfc81b5f8a10dab8603
sh3ev <sheevron@gmail.com>
2014-04-08 22:17:00 +0200

    APP added

--- Beeather/settings.py
+++ Beeather/settings.py
@@ -36,7 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'weather',
 )
+
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
